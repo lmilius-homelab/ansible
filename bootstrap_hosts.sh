c5267412a25c43ab18ad02d1e34481e21bbfee2b
Luke Milius <lmilius12@gmail.com>
2022-07-01 23:29:35 -0500

    Update for use with proxmox os

--- bootstrap_hosts.sh
+++ bootstrap_hosts.sh
@@ -2,4 +2,4 @@
 
 # To override the ansible user, add `-e "ansible_user=username"` when calling this script to use a user other than the default
 
-ansible-playbook envsetup.yml -K --ask-pass $@
+ansible-playbook envsetup.yml --ask-pass $@
