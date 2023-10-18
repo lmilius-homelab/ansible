ecd528e33d5b0902d6da721214b9f34e76380dbe
Luke Milius <lmilius12@gmail.com>
2023-10-17 20:26:20 -0500

    Variblize the things

--- setup_ansible_host.sh
+++ setup_ansible_host.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
 sudo apt-get update &&
-sudo apt-get install software-properties-common -y
+sudo apt-get install software-properties-common terraform -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 
 sudo apt-get update &&
---
+++ unlock_bw.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export BW_SESSION=$(bw unlock --raw)
