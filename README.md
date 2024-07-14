362a5e0776ea86b9e1ce67052a2d9bb87fadecae
Luke Milius <lmilius12@gmail.com>
2024-07-14 18:19:31 -0500

    support nix host

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ansible
 Repository containing all host configuration IaC using Ansible
+
+## SSH Key Setup
+
+`ansible-vault view vars/ssh_key > ~/.ssh/ansible_key`
+
+`chmod 600 ~/.ssh/ansible_key`
+
+## Nix
+
+`nix develop .`
+
+`source unlock_bw.sh`
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Ansible host Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              just
+              ansible
+              bitwarden-cli
+            ];
+            shellHook = ''
+              ansbile-galaxy install -r requirements.txt --force
+            '';
+          };
+        }
+    );
+}
